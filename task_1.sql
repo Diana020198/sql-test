093026ae13b368ed1231ebd69a2606e6b21282d0
Diana020198 <platonovadiana2019@yandex.ru>
2026-07-30 21:46:30 +0300

    Update task_1.sql
    Add task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,2 +1,16 @@
-SELECT *
-FROM users;
+SELECT
+    u.id,
+    u.username,
+    STRING_AGG(DISTINCT ur.role, ', ') AS roles,
+    COUNT(ua.id) AS activity_count
+FROM users u
+LEFT JOIN user_roles ur
+    ON u.id = ur.user_id
+LEFT JOIN user_activity ua
+    ON u.id = ua.user_id
+    AND ua.activity_date >= CURRENT_DATE - INTERVAL '1 month'
+GROUP BY
+    u.id,
+    u.username
+HAVING COUNT(ua.id) > 0
+ORDER BY activity_count DESC;
